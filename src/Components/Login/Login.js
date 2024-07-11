ff7c4f5713938e931f5e0b5e64f22a023a9426c0
ellen-ls <ellen.l.s1996@gmail.com>
2024-07-11 20:08:35 -0300

    ajeitando o login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,6 +5,7 @@ import { FaLock } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
+import logo from '../shared/Img/logo-financeiro.png'
 
 export default function Login() {
 
@@ -35,14 +36,17 @@ export default function Login() {
         
           {activePanel === 'signin' && (
             <div id="panel-1"  className="space-y-8">
-              <h1 className='text-black text-2xl font-bold'>Operação Financeira</h1>
+              <div className='flex gap-2 justify-center items-center'>
+              <img src={logo} alt='' className='size-10 rounded-2xl'></img>
+              <h1 className='text-xl font-bold'>Operação Financeira</h1>
+              </div>
               <form className='space-y-4 '>
-                <div className="field flex items-center justify-between border-b border-sky-700 relative px-2 text-black">
+                <div className="field flex items-center justify-between border-b border-sky-700 relative px-2 ">
                   <input type="text" id="name-1" placeholder="&nbsp;" className="peer bg-transparent text-sm py-3 w-full outline-none ring-0" required />
                   <label htmlFor="name-1" className="absolute font-thin transition-all duration-300 -translate-y-6 text-sm peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Name</label>
                   <span className="material-symbols-outlined"><FaUser /></span>
                 </div>
-                <div className="field flex items-center justify-between border-b border-sky-700 relative px-2 text-black">
+                <div className="field flex items-center justify-between border-b border-sky-700 relative px-2 ">
                   <input type="password" id="pass-1" placeholder="&nbsp;" className="peer bg-transparent text-sm py-3 w-full outline-none ring-0" required />
                   <label htmlFor="pass-1" className="absolute font-thin transition-all duration-300 -translate-y-6 text-sm peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Password</label>
                   <span className="material-symbols-outlined"><FaLock /></span>
@@ -55,19 +59,31 @@ export default function Login() {
                         <span className="material-symbols-outlined"></span>
                         <span className="material-symbols-outlined"></span>
                       </div>
-                      <p className='text-black'>Remember Me</p>
+                      <p>Remember Me</p>
                     </label>
                   </div>
-                  <a className="text-xs text-black">Forgot Password</a>
+                  <a className="text-xs ">Forgot Password</a>
                 </div>
-                <button type="button" className="uppercase text-lg py-3 px-4 bg-sky-700 text-white relative isolate w-full flex items-center justify-center gap-2 overflow-hidden ring-0 outline-none before:absolute before:inset-0 before:-z-10 before:opacity-0 before:transition-all before:duration-300 before:ease-out before:bg-[#2f285a] hover:before:opacity-100 focus-visible:before:opacity-100"
+                <button type="button" className="uppercase text-lg py-3 px-4 bg-sky-700
+                 text-white relative isolate w-full flex items-center justify-center gap-2 
+                 overflow-hidden ring-0 outline-none before:absolute before:inset-0 before:-z-10 
+                 before:opacity-0 before:transition-all before:duration-300 before:ease-out 
+                 before:bg-[#2f285a] hover:before:opacity-100 focus-visible:before:opacity-100 rounded-xl"
                 onClick={()=>navigate('/usuario')}
                 >
                   Log in
                 </button>
               </form>
-              <p className="spacer-or mx-auto w-2/4 flex items-center justify-center gap-2 text-sm whitespace-nowrap before:block before:bg-gradient-to-r before:to-[#0056FE] before:w-2/4 before:h-px after:block after:bg-gradient-to-l after:to-[#2E3CBD] after:w-2/4 after:h-px text-black">or</p>
-              <button type="button" className="!bg-[#4267B2] py-3 px-4 bg-sky-700 text-white relative isolate w-full flex items-center justify-center gap-2 overflow-hidden ring-0 outline-none before:absolute before:inset-0 before:-z-10 before:opacity-0 before:transition-all before:duration-300 before:ease-out before:bg-[#2f285a] hover:before:opacity-100 focus-visible:before:opacity-100">
+              <p className="spacer-or mx-auto w-2/4 flex items-center 
+              justify-center gap-2 text-sm whitespace-nowrap before:block 
+              before:bg-gradient-to-r before:to-[#0056FE] before:w-2/4 
+              before:h-px after:block after:bg-gradient-to-l 
+              after:to-[#2E3CBD] after:w-2/4 after:h-px">or</p>
+              <button type="button" className="!bg-[#4267B2] py-3 px-4 bg-sky-700 
+              text-white relative isolate w-full flex items-center justify-center gap-2 
+              overflow-hidden ring-0 outline-none before:absolute before:inset-0 before:-z-10 
+              before:opacity-0 before:transition-all before:duration-300 before:ease-out 
+              before:bg-[#2f285a] hover:before:opacity-100 focus-visible:before:opacity-100 rounded-xl">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-8">
                   <path fill="currentColor" d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4z" />
                 </svg>
@@ -78,28 +94,43 @@ export default function Login() {
           {activePanel === 'signup' && (
             <div id="panel-2" className="space-y-8">
               <form className="space-y-4">
-              <h1 className='text-black text-2xl font-bold'>Operação Financeira</h1>
-                <div className="field flex items-center justify-between border-b border-sky-700 relative px-2 text-black">
+                <div className='flex gap-2 justify-center items-center'>
+                 <img src={logo} alt='' className='size-10 rounded-2xl'></img>
+                <h1 className='text-xl font-bold'>Operação Financeira</h1>
+              </div>
+                <div className="field flex items-center justify-between border-b border-sky-700 relative px-2 ">
                   <input type="text" id="name-2" placeholder="&nbsp;" className="peer bg-transparent text-sm py-3 w-full outline-none ring-0" required />
-                  <label htmlFor="name-2" className="absolute font-thin transition-all duration-300 -translate-y-6 text-sm peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Name</label>
+                  <label htmlFor="name-2" className="absolute font-thin transition-all 
+                  duration-300 -translate-y-6 text-sm peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Name</label>
                   <span className="material-symbols-outlined"><FaUser /></span>
                 </div>
-                <div className="field flex items-center justify-between border-b border-sky-700 relative px-2 text-black">
+                <div className="field flex items-center justify-between border-b border-sky-700 relative px-2 ">
                   <input type="email" id="email-2" placeholder="&nbsp;" className="peer bg-transparent text-sm py-3 w-full outline-none ring-0" required  />
-                  <label htmlFor="email-2" className="absolute font-thin transition-all duration-300 -translate-y-6 text-sm peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Email</label>
+                  <label htmlFor="email-2" className="absolute font-thin transition-all 
+                  duration-300 -translate-y-6 text-sm peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Email</label>
                   <span className="material-symbols-outlined"><MdEmail /></span>
                 </div>
-                <div className="field flex items-center justify-between border-b border-sky-700 relative px-2 text-black">
+                <div className="field flex items-center justify-between border-b border-sky-700 relative px-2 ">
                   <input type="password" id="pass-2" placeholder="&nbsp;" className="peer bg-transparent text-sm py-3 w-full outline-none ring-0" required />
-                  <label htmlFor="pass-2" className="absolute font-thin transition-all duration-300 -translate-y-6 text-sm peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Password</label>
+                  <label htmlFor="pass-2" className="absolute font-thin transition-all 
+                  duration-300 -translate-y-6 text-sm peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-6">Password</label>
                   <span className="material-symbols-outlined"><FaLock /></span>
                 </div>
-                <button type="button" className="uppercase text-lg py-3 px-4 bg-sky-700 text-white relative isolate w-full flex items-center justify-center gap-2 overflow-hidden ring-0 outline-none before:absolute before:inset-0 before:-z-10 before:opacity-0 before:transition-all before:duration-300 before:ease-out before:bg-[#2f285a] hover:before:opacity-100 focus-visible:before:opacity-100">
+                <button type="button" className="uppercase text-lg py-3 px-4 bg-sky-700 
+                text-white relative isolate w-full flex items-center justify-center gap-2 
+                overflow-hidden ring-0 outline-none before:absolute before:inset-0 before:-z-10 
+                before:opacity-0 before:transition-all before:duration-300 before:ease-out 
+                before:bg-[#2f285a] hover:before:opacity-100 focus-visible:before:opacity-100 rounded-xl">
                   Sign up
                 </button>
               </form>
-              <p className="spacer-or mx-auto w-2/4 flex items-center justify-center gap-2 text-sm whitespace-nowrap before:block before:bg-gradient-to-r before:to-[#0056FE] before:w-2/4 before:h-px after:block after:bg-gradient-to-l after:to-[#2E3CBD] after:w-2/4 after:h-px text-black">or</p>
-              <button type="button" className="!bg-[#4267B2] py-3 px-4 bg-sky-700 text-white relative isolate w-full flex items-center justify-center gap-2 overflow-hidden ring-0 outline-none before:absolute before:inset-0 before:-z-10 before:opacity-0 before:transition-all before:duration-300 before:ease-out before:bg-[#2f285a] hover:before:opacity-100 focus-visible:before:opacity-100">
+              <p className="spacer-or mx-auto w-2/4 flex items-center justify-center gap-2 text-sm 
+              whitespace-nowrap before:block before:bg-gradient-to-r before:to-[#0056FE] 
+              before:w-2/4 before:h-px after:block after:bg-gradient-to-l after:to-[#2E3CBD] after:w-2/4 after:h-px ">or</p>
+              <button type="button" className="!bg-[#4267B2] py-3 px-4 bg-sky-700 text-white 
+              relative isolate w-full flex items-center justify-center gap-2 overflow-hidden ring-0 
+              outline-none before:absolute before:inset-0 before:-z-10 before:opacity-0 before:transition-all 
+              before:duration-300 before:ease-out before:bg-[#2f285a] hover:before:opacity-100 focus-visible:before:opacity-100 rounded-xl">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-8">
                   <path fill="currentColor" d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4z" />
                 </svg>
